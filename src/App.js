7fa31d8829655a4efdb7a795d0b0ccd31232b90c
Aniket Sharma <aniketsharma312@gmail.com>
2021-09-08 22:52:49 -0600

    Made changes to css to center align items

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
-import Background from './Components/Background'
+import Background from './Components/Background.js'
 
 function App(props) {
   return (
     <div className="App"> 
-
+      <Background />
     </div>
   );
 }
 
 export default App;
+
--- src/Components/Background.js
+++ src/Components/Background.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import './Background.css'
 
 class Background extends React.Component {
     render() {
-        return 
-        <div className="background" >
+        return(  
+            <div className="background" >
             <h1>Gradient Text</h1>
-        </div>
+            </div>
+        )
     }
 }
 
