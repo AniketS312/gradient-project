cceb01eec67650706c99fe65df0ed8febf932b0e
Aniket Sharma <aniketsharma312@gmail.com>
2021-09-09 21:03:38 -0700

    started working on file

--- src/Components/Background.js
+++ src/Components/Background.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './Background.css'
 
+// const unsplashImage = `https://source.unsplash.com/collection/4760062/1600x900`
+
 class Background extends React.Component {
     render() {
         return(  
@@ -8,7 +10,7 @@ class Background extends React.Component {
             <h1>Gradient Text</h1>
             </div>
         )
-    }
+}
 }
 
 export default Background;
